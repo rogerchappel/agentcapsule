177b74d580f33309284ca0f8b569d1860d4b852f
Roger Chappel <miscanalysis@gmail.com>
2026-06-29 10:12:35 +1000

    Add capsule planning API

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 export { CONFIG_FILE, DEFAULT_CONFIG, DEFAULT_EXCLUDES, DEFAULT_SECRET_DENY, initConfig, loadConfig } from "./config.js";
+export { checkCapsule, validateManifest } from "./check.js";
 export { inspectCapsule } from "./inspect.js";
 export { packCapsule } from "./pack.js";
+export { planCapsule } from "./plan.js";
 export { unpackCapsule } from "./unpack.js";
 export { isUnsafeEntryPath, safeJoin, selectFiles } from "./files.js";
 export type {
+  CapsuleCheck,
   CapsuleCommand,
   CapsuleConfig,
   CapsuleManifest,
+  CapsulePlan,
   CommandReceipt,
   ManifestFile,
   PackResult,
